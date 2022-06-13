fa8d39dc9fd4a575d08509d94a2c1e3c574b81aa
Diana-2604 <Diana8991@yandex.ru>
2022-06-13 17:32:47 +0300

    add Lesson002_Task15

---
+++ Lesson002_Task15/Program.cs
@@ -0,0 +1,21 @@
+﻿/* Задача 15: Напишите программу, которая принимает на
+вход цифру, обозначающую день недели, и проверяет,
+является ли этот день выходным.
+6 -> да
+7 -> да
+1 -> нет */
+
+Console.WriteLine("Is this day a weekend?");
+
+int day = new Random().Next(1,8);
+
+Console.WriteLine(day);
+
+if (day >= 1 && day < 6)
+{
+    Console.WriteLine("no");
+}
+else
+{
+    Console.WriteLine("yes");
+}
