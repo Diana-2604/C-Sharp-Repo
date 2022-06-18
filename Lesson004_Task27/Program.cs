df5678597b178d4731a94792209905692efd81cd
Diana-2604 <Diana8991@yandex.ru>
2022-06-18 19:10:42 +0300

    add Lesson004_Task27

---
+++ Lesson004_Task27/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на
+// вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int Sum(int number)
+{
+    int result = 0;
+
+    string array = number.ToString();
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        result += int.Parse(array[i].ToString());
+    }
+    return result;
+}
+
+Console.WriteLine($"452 -> {Sum(452)}");
+Console.WriteLine($"82 -> {Sum(82)}");
+Console.WriteLine($"9012 -> {Sum(9012)}");
