16f36d9ba1f3c5781cbd1d924d5f1f7789d2ed06
Diana-2604 <Diana8991@yandex.ru>
2022-06-18 20:57:35 +0300

    add Lesson004_Task29

---
+++ Lesson004_Task29/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая задаёт массив
+// из 8 элементов и выводит их на экран.
+
+int[] array = new int[8];
+
+void FillArray (int[] collection)
+{
+    int length = collection.Length;
+    for (int i = 0; i < length; i++)
+    {
+        collection[i] = new Random().Next(1,10);
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+
+    Console.Write("{ ");
+
+    for (int pos = 0; pos < count; pos++)
+    {
+        Console.Write($"{col[pos]} ");
+    }
+
+    Console.Write("}");
+}
+
+FillArray(array);
+PrintArray(array);
+
